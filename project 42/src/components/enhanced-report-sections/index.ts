485c29e9949d48a89b6cc0335b94db20206bfa66
felixgbondatpropnition <felixgbond@gmail.com>
2025-04-25 19:59:30 +0100

    Delete project 42 directory

--- project 42/src/components/enhanced-report-sections/index.ts
+++
@@ -1,5 +0,0 @@
-export { AdvancedFinancialSection } from './AdvancedFinancialSection';
-export { MarketAnalysisSection } from './MarketAnalysisSection';
-export { RiskAnalysisSection } from './RiskAnalysisSection';
-export { TokenizationAnalysisSection } from './TokenizationAnalysisSection';
-export { JurisdictionalAnalysisSection } from './JurisdictionalAnalysisSection';
